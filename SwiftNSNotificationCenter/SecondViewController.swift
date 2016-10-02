f6a429635d3f306a1c9c62bc70ad16aa0523d4ed
amglobal99 <amglobal99@yahoo.com>
2016-10-02 09:41:33 -0400

    Initial Commit .... Working copy.... Swift 3

---
+++ SwiftNSNotificationCenter/SecondViewController.swift
@@ -0,0 +1,17 @@
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+	@IBOutlet weak var notificationLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+        
+		NotificationCenter.default.addObserver(self, selector: #selector(SecondViewController.actOnSpecialNotification), name: NSNotification.Name(rawValue: mySpecialNotificationKey), object: nil)
+	}
+	
+	func actOnSpecialNotification() {
+		self.notificationLabel.text = "I heard the notification!"
+	}
+}
---
+++ SwiftNSNotificationCenter/ThirdViewController.swift
@@ -0,0 +1,16 @@
+import Foundation
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+	@IBOutlet weak var notificationLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		NotificationCenter.default.addObserver(self, selector: #selector(ThirdViewController.actOnSpecialNotification), name: NSNotification.Name(rawValue: mySpecialNotificationKey), object: nil)
+	}
+	
+	func actOnSpecialNotification() {
+		self.notificationLabel.text = "I heard the notification, too!"
+	}
+}
